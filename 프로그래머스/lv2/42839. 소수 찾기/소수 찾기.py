95a42d284f919b34e8ea9a413a6427e7cb3bff7b
Juchan Jang <32419551+foundaway98@users.noreply.github.com>
2023-09-30 22:42:00 +0900

    [level 2] Title: 소수 찾기, Time: 0.20 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,5 +1,4 @@
 from itertools import permutations
-import math
 def solution(numbers):
     answer = 0
     numlist = []
@@ -7,21 +6,18 @@ def solution(numbers):
     for i in range(1,len(numbers)+1):
         perList = permutations(numbers,i)
         for perm in perList:
+            print(perm)
             num = int(''.join(perm))
             allNums.add(num)
-    for n in allNums:
-        isAnswer = 0
-        if n == 0 or n == 1:
-            continue
-        elif n == 2:
-            answer += 1
-            continue
-        for i in range(2,math.ceil(math.sqrt(n))+1):
-            if n%i == 0:
-                isAnswer = 1
-                break
-        if isAnswer == 1:
-            continue
-        else:
-            answer +=1
+            
+    print(allNums)
+    
+                
+            
+        
+    
+    # 조합 생성 후
+    
+    # 조합된 숫자들 소수 판별
+    
     return answer
