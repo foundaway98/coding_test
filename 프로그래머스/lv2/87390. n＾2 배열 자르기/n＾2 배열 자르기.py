a88625640b6ac211c13bfb5d111b07f9654e6585
Juchan Jang <32419551+foundaway98@users.noreply.github.com>
2023-10-03 01:45:54 +0900

    [level 2] Title: n^2 배열 자르기, Time: 49.76 ms, Memory: 24.5 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,13 +1,3 @@
 def solution(n, left, right):
-    # index 와 숫자 사이의 관계 : index // n과 index % n 중 큰 수에 + 1을 한 것과 같다
-    matrix = []
     answer = []
-    for num in range(left, right+1):
-        row = num//n + 1
-        col = num%n + 1
-        matrix.append([row, col])
-    
-    for mat in matrix:
-        answer.append(max(mat[0], mat[1]))
-
     return answer
