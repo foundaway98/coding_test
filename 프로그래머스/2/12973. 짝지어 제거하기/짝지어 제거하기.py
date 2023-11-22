0b649997a243100a53ef7daba4f40fc86c0048d0
Juchan Jang <32419551+foundaway98@users.noreply.github.com>
2023-11-22 16:58:00 +0900

    [level 2] Title: 짝지어 제거하기, Time: 143.88 ms, Memory: 22.4 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -12,9 +12,5 @@ def solution(s):
             if stack[-1] == stack[-2]:
                 stack.pop()
                 stack.pop()
-    if len(stack) == 0:
-        answer = 1
-    else:
-        answer = 0
     
-    return answer
+    return 1 if len(stack) == 0 else 0
