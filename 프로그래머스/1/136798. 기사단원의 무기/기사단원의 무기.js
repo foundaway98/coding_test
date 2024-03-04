1d921c2cd410351b8baedbf9de83a25f9a6e0cdc
Juchan Jang <32419551+foundaway98@users.noreply.github.com>
2024-03-04 18:30:08 +0900

    [level 1] Title: 기사단원의 무기, Time: 7.33 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,31 @@
+function solution(number, limit, power) {
+    var answer = 0;
+    let knights = [1]
+    
+    for(let knight=2;knight<=number;knight++){
+        cnt = 0
+        for(let i = 1; i<=parseInt(Math.sqrt(knight));i++){
+            if(knight % i == 0){
+                cnt += 1
+            }
+        }
+        cnt *= 2
+        
+        if(Math.sqrt(knight) - parseInt(Math.sqrt(knight)) === 0){
+            cnt -= 1
+        }
+        if(cnt > limit){
+            cnt = power
+        }
+        
+        knights.push(cnt)
+    }
+    console.log(knights)
+    
+    
+    for(let p of knights){
+        answer += p
+    }
+    
+    return answer;
+}
