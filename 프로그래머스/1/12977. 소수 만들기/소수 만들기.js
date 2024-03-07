2e7f0743600aa87e33a35c4c925dbc11e1a05ea1
Juchan Jang <32419551+foundaway98@users.noreply.github.com>
2024-03-08 01:21:08 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,23 @@
+function solution(nums) {
+    var answer = 0;
+    let len = nums.length
+    for(let i = 0; i<len-2;i++){
+        for(let j = i+1; j<len-1; j++){
+            for(let k = j+1; k<len; k++){
+                if(isPrime(nums[i]+nums[j]+nums[k])){
+                    answer += 1;
+                }
+            }
+        }
+    }
+    return answer;
+}
+
+function isPrime(num){
+    for(let i = 2; i<=Math.sqrt(num); i++){
+        if(num % i === 0){
+            return false
+        }
+    }
+    return true
+}
